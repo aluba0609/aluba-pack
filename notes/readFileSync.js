598e35bbc0735e6c913aaab15531ce5a3e6eb73b
aluba0609 <164868966@qq.com>
2021-06-11 16:35:18 +0800

    1

--- notes/readFileSync.js
+++ notes/readFileSync.js
@@ -1,4 +1,10 @@
 let fs=require('fs')
-let path='D:/项目/vue/git/webpack/src/index.js'
-let content=fs.readFileSync(path,'utf8')//反斜杠会报错
-console.log(content)
+
+// let mypath1='D:\项目\vue\git\webpack\src\index.js'
+let mypath2='D:/项目/vue/git/webpack/src/index.js'
+let content=fs.readFileSync(mypath2,'utf8')//反斜杠会报错
+console.log('content:',content)
+
+let path=require('path')
+let dirnamepath=path.dirname(mypath2)
+console.log('dirnamepath:',dirnamepath)
