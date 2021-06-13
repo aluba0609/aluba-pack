a1b3b578d904dd76c0ccd4c9b4dd25708df6361a
aluba0609 <164868966@qq.com>
2021-06-13 10:57:20 +0800

    1

--- bin/aluba-pack.js
+++ bin/aluba-pack.js
@@ -7,8 +7,10 @@ let path=require('path');
 //config配置文件
 let config=require(path.resolve('webpack.config.js'));
 
-let Compiler=require('../lib/Compiler.js')
+// let Compiler=require('../lib/Compiler.js')
+let Compiler=require('../main/main.js')
+
 let compiler = new Compiler(config);
-compiler.hooks.entryOption.call();
+// compiler.hooks.entryOption.call();
 //表示运行编译
 compiler.run()
--- main/main.js
+++ main/main.js
@@ -1,16 +1,43 @@
 let path=require('path');
 let fs=require('fs');
-const { parse } = require('path');
+let babylon = require('babylon');
+let traverse = require('@babel/traverse').default;
+let t = require('@babel/types');
+let generator = require('@babel/generator').default;
+
 class Compiler{
     constructor(config){
         this.config=config;
         this.entryId;
         this.entry=config.entry.index;
+        this.modules={};
         this.root=process.cwd()
     }
     getSource(modulePath){
-        let content=fs.readFileSync(modulePath);
+        let content=fs.readFileSync(modulePath,'utf8');
         return content
+    }
+    parse(source,parentPath){
+        let ast=babylon.parse(source)
+        let dependencies=[];
+        traverse(ast,{
+            CallExpression(p){
+                let node = p.node;
+                if(node.callee.name ==='require'){
+                    node.callee.name='__webpack_require__';
+                    let moduleName = node.arguments[0].value;
+                    moduleName=moduleName+(path.extname(moduleName)?'':'.js');
+                    moduleName='./'+path.join(parentPath,moduleName);
+                    dependencies.push(moduleName);
+                    node.arguments=[t.stringLiteral(moduleName)]
+                    // console.log('+++++++++++++',node.arguments)
+                }
+                
+            }
+        })
+        let sourceCode=generator(ast).code;
+        console.log('+++++++++++++',sourceCode)
+        return{sourceCode,dependencies}
     }
     buildModule(modulePath,isEntry){
         let source=this.getSource(modulePath)
@@ -20,6 +47,11 @@ class Compiler{
         }
         let {sourceCode,dependencies}=this.parse(source,path.dirname(moduleName))
         this.modules[moduleName]=sourceCode
+        dependencies.forEach((dep)=>{
+            // console.log(path.join(this.root,dep))
+            this.buildModule(path.join(this.root,dep),false)
+        })
+        // console.log(this.modules)
     }
     run(){
         this.buildModule(path.resolve(this.root,this.entry),true)
